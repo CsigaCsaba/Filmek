de85822b4b617b2ebed22402cb3a3c11e2c9e3f0
CsigaCsaba <horvathcsabapeter@turr.hu>
2026-04-16 19:15:03 +0200

    Villámgyors feltöltés videó nélkül

---
+++ script.js
@@ -0,0 +1,34 @@
+let currentFileName = "";
+
+function openModal(title, desc, imgSrc, rating, fileName) {
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalDescription").innerText = desc;
+    document.getElementById("modalImg").src = imgSrc;
+    document.getElementById("modalRating").innerText = "Értékelés: " + rating;
+    document.getElementById("modalFileName").innerText = fileName;
+    
+    currentFileName = fileName; // Eltároljuk a fájlnevet a lejátszáshoz
+    
+    document.getElementById("movieModal").style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+function closeModal() {
+    document.getElementById("movieModal").style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+// A tényleges lejátszás függvénye
+function startMovie() {
+    if (currentFileName) {
+        // Megnyitja a fájlt (ha egy mappában van a HTML-el)
+        window.open(currentFileName, '_blank');
+    }
+}
+
+window.onclick = function(event) {
+    let modal = document.getElementById("movieModal");
+    if (event.target == modal) {
+        closeModal();
+    }
+}
